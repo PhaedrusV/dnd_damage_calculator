035c7b176e2666ad52f320209411702204d96791
Nick Allen <42513322+PhaedrusV@users.noreply.github.com>
2025-03-02 23:33:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ def index():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(host='0.0.0.0', port=port, debug=False)
 """
 from flask import Flask, render_template, request
 import plotly.graph_objects as go
